22c53f332548768d931472797fbcc96e2d240b05
8374611072Ram <p225rameshas@gmail.com>
2022-11-08 21:01:40 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from app.fomrs import *
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
@@ -11,23 +14,42 @@ def registration(request):
     uf=UserForm()
     pf=ProfileForm()
     d={'uf':uf,'pf':pf}
+
     if request.method=='POST' and request.FILES:
         ud=UserForm(request.POST)
         pd=ProfileForm(request.POST,request.FILES)
         if ud.is_valid() and pd.is_valid():
-           UI=ud.save(commit=False)
-           pw=ud.cleaned_data.get('password')
-           UI.set_password(pw)
-           UI.save()
-           PI=pd.save(commit=False)
-           PI.user=UI
-           PI.save()
-           send_mail('registration',
-           'Hi bro had you completed your dinner',
-           'ramperupogu@gmail.com',
-           [UI.email],
-           fail_silently=False)
-
-           return HttpResponse('Registration is Success')
-
+            UI=ud.save(commit=False)
+            pw=ud.cleaned_data.get('password')
+            UI.set_password(pw)
+            UI.save()
+            PI=pd.save(commit=False)
+            PI.user=UI
+            PI.save()
+            send_mail('registration',
+                'Successfully Registered',
+                'harshadvali1431@gmail.com',
+                [UI.email],
+                fail_silently=False)
+            return HttpResponse('Registration is Success')         
     return render(request,'registration.html',d)
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user and user.is_active:
+            login(request, user)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid User')
+
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
--- project20/urls.py
+++ project20/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',home,name='home'),
     path('registration/',registration,name='registration'),
+    path('user_login/',user_login,name='user_login'),
+    path('user_logout/',user_logout,name='user_logout'),
 
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
