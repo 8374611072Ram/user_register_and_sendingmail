1ee17fa1cc6a0cef95cfd7dc84c3dc53333c4410
8374611072Ram <p225rameshas@gmail.com>
2022-11-09 22:10:30 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from django.core.mail import send_mail
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from app.models import *
 
 # Create your views here.
 def home(request):
@@ -27,8 +28,8 @@ def registration(request):
             PI.user=UI
             PI.save()
             send_mail('registration',
-                'Successfully Registered',
-                'harshadvali1431@gmail.com',
+                'I Love you Baby Thinva And nuvu nakosam epudu vastavu',
+                'ramperupogu@gmail.com',
                 [UI.email],
                 fail_silently=False)
             return HttpResponse('Registration is Success')         
@@ -53,3 +54,33 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('home'))
+
+
+@login_required
+def display_profile(request):
+    username=request.session.get('username')
+    UO=User.objects.get(username=username)
+    PO=Profile.objects.get(user=UO)
+    d={'UO':UO,'PO':PO}
+    return render(request,'display_profile.html',d)
+
+@login_required
+def change_password(request):
+    if request.method=='POST':
+        username=request.session.get('username')
+        uo=User.objects.get(username=username)
+        pw=request.POST['password']
+        uo.set_password(pw)
+        uo.save()
+        return HttpResponse('Password is changed Successfully')
+    return render(request,'change_password.html')
+
+def reset_password(request):
+    if request.method=='POST':
+        un1=request.POST['username']
+        pw1=request.POST['password']
+        UO1=User.objects.get(username=un1)
+        UO1.set_password(pw1)
+        UO1.save()
+        return HttpResponse('Reset password is suucessful')
+    return render(request,'reset_password.html')
